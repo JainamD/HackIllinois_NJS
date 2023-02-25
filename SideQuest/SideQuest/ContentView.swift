fd7f2f567a4a7922b116907a7c5b4ea38d208952
Siya Digra <siyadigra@gmail.com>
2023-02-25 01:56:54 -0600

    Created title screen

--- SideQuest/SideQuest/ContentView.swift
+++ SideQuest/SideQuest/ContentView.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Text("Side Quest")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 30)
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .padding(.bottom, 30)
+                Button(action: {
+                    // handle button tap
+                }) {
+                    Text("Start")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 50)
+                        .padding(.vertical, 15)
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+            }
+            .padding(.top, 100)
         }
-        .padding()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
