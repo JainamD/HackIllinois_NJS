b6a4f196eb5ae2b9e273ee5e15ea7440cbaff7a0
Siya Digra <siyadigra@gmail.com>
2023-02-25 01:32:54 -0600

    Added app framework

---
+++ HackIllinois/HackIllinois/HackIllinoisApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackIllinoisApp.swift
+//  HackIllinois
+//
+//  Created by Siya Digra on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct HackIllinoisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
