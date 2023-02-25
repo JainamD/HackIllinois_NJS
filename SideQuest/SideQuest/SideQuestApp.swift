faf9a749315d05ab1ee1fd7e433ca088f6718b65
Siya Digra <siyadigra@gmail.com>
2023-02-25 01:51:48 -0600

    using swiftui instead

---
+++ SideQuest/SideQuest/SideQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideQuestApp.swift
+//  SideQuest
+//
+//  Created by Siya Digra on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SideQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
