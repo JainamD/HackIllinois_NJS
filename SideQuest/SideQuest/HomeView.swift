06d6bff97aaa346e306d03d2f4f8e6d583e52681
NishkaAwasthi <nishka@habu.com>
2023-02-26 05:53:02 -0600

    home view is added

---
+++ SideQuest/SideQuest/HomeView.swift
@@ -0,0 +1,77 @@
+//
+//  HomeView.swift
+//  SideQuest
+//
+//  Created by Nishka Awasthi on 2/26/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var register: Bool = false
+    @State private var login: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Image("other_tree_bg") // replace "your-background-image-name" with your actual image name
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .edgesIgnoringSafeArea(.all)
+            VStack {
+//                Text("Welcome to \nSide Quest!")
+//                    .font(.largeTitle)
+//                    .fontWeight(.bold)
+//                    .padding(.bottom, 30)
+                Image("sidequestlogo2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 400)
+                    .padding(.bottom, 20)
+                    .padding(.top, -450)
+                Button("Recieve Quest") {
+                    self.login = true
+                }.font(.title)
+                    .fontWeight(.bold)
+                    .padding(.horizontal, 50)
+                    .padding(.vertical, 15)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    .sheet(isPresented: $login, content: {
+                    LoginView()
+                })
+                Button("Add New Goals") {
+                    self.register = true
+                }.font(.title)
+                    .fontWeight(.bold)
+                    .padding(.horizontal, 50)
+                    .padding(.vertical, 15)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    .sheet(isPresented: $register, content: {
+                    RegisterView()
+                })
+                Button("How to use") {
+                    self.register = true
+                }.font(.title)
+                    .fontWeight(.bold)
+                    .padding(.horizontal, 50)
+                    .padding(.vertical, 15)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                    .sheet(isPresented: $register, content: {
+                    RegisterView()
+                })
+            }
+            .padding(.top, 100)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
