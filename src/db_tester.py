6992bafd99b00f15a03b87ae52348fce1b98fca6
NishkaAwasthi <nishka@habu.com>
2023-02-26 02:03:03 -0600

    removing duplicated pot_sq.db

--- src/db_tester.py
+++ src/db_tester.py
@@ -10,6 +10,7 @@ person.add_quest('"Clean Closet"', '"Organize everything in my closet"', 0, 0, 0
 person.add_quest('"Make a Dessert"', '"Find a recipe online on youtube and make it"', 1, 1, 0, 1, 1, 1, 20, 0, '"Jainam"')
 person.add_quest('"Call my friends"', '"Reach out to my highschool friends"', 1, 1, 1, 0, 1, 0, 60, 0, '"Nishka"')
 person.add_quest('"Go to Taco Bell"', '"Get food so J does not kill me"', 0, 0, 0, 0, 0, 1, 45, 0, '"Nishka"')
-person.add_quest('"Go to Mexican Restaurant"', '"Get food so J does not kill me"', 0, 0, 0, 0, 0, 1, 45, 0, '"Nishka"')
+person.add_quest('"Go to Mexican"', '"Get food so J does not kill me"', 0, 0, 0, 0, 0, 1, 45, 0, '"Nishka"')
 
 
+#person.delete_quest_name('"Clean Closet"', '"Jainam"')
