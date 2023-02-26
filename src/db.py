8d8ec6c99ef98d4438793578f0aa0437e4c7318c
JainamD <89661428+JainamD@users.noreply.github.com>
2023-02-25 19:40:25 -0600

    changing format for db table queries

--- src/db.py
+++ src/db.py
@@ -3,7 +3,6 @@ import sqlite3
 # class that represents the data you want to expose through the AP
 class db:
     def __init__(self, name):
-        self.name = name
 
     # Create Table Method (Only excecute to create a fresh table)
     def create_tables(self):
@@ -27,7 +26,8 @@ class db:
                     Bored INT NOT NULL,
                     Hungry INT NOT NULL,
                     Minutes INT NOT NULL,
-                    Finished INT NOT NULL);'''
+                    Finished INT NOT NULL,
+                    Username TEXT NOT NULL);'''
         conn.execute(s)
         s = 'CREATE TABLE uesrs (name TEXT PRIMARY KEY)'
         conn.execute(s)
@@ -37,24 +37,24 @@ class db:
         conn.close()
 
     # Add Side Quest
-    def add_quest(self, N, D, Ha, S, T, M, B, Hn, Mins, Fin):
+    def add_quest(self, N, D, Ha, S, T, M, B, Hn, Mins, Fin, user):
         conn = sqlite3.connect('potential_sidequests.db')
 
         # Check Name is not taken
         cursor = conn.cursor()
-        s = 'SELECT COUNT(*) FROM sidequests_{} WHERE Name = {}'.format(self.name, N)
+        s = 'SELECT COUNT(*) FROM sidequests WHERE Name = {} AND Username = {}'.format(N, user)
         cursor.execute(s)
         count = cursor.fetchone()[0]
 
         if count != 0:
             conn.close()
-            print('NAME TAKEN BISH')
+            #print('NAME TAKEN BISH')
             return('Name taken')
 
         # Insert data into the table
         s = 'INSERT INTO sidequests_{}'.format(self.name)
-        s += ' (Name, Description, Happy, Sad, Tired, Motivated, Bored, Hungry, Minutes, Finished) '
-        s += "VALUES ({}, {}, {}, {}, {}, {}, {}, {}, {}, {})".format(N, D, Ha, S, T, M, B, Hn, Mins, Fin)
+        s += ' (Name, Description, Happy, Sad, Tired, Motivated, Bored, Hungry, Minutes, Finished, Username) '
+        s += "VALUES ({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})".format(N, D, Ha, S, T, M, B, Hn, Mins, Fin, user)
         conn.execute(s)
 
         # Commit changes and close the connection
